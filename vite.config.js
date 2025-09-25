40c0c09d191e3434a7a5b77fa4c3ea98579cbe3d
rachaspire <rachiket.a@aspireapp.com>
2025-09-25 04:00:23 +0000

    allow replit url in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5000,
     strictPort: true,
     hmr: {
-      clientPort: 443
-    }
-  }
-})
+      clientPort: 443,
+    },
+    allowedHosts: [
+      "2ea3627b-a371-4f86-b466-0846b9c99a33-00-n53dyd1u8gu7.sisko.replit.dev",
+    ],
+  },
+});
